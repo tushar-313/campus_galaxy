10ea92ede4050d0f3aaddacd3b673f35c0453bf1
Tushar Agarwal <tusharagarwal@Tushars-MacBook-Air.local>
2026-01-20 00:18:48 +0530

    cloud db

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: 'localhost',       
-  user: 'root',          
-  database: 'campus_db',
-  password: 'Kenshu$202' 
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 
 connection.connect((err) => {
   if (err) {
-    console.log('DB connection error:', err);
+    console.log("DB connection error:", err);
   } else {
-    console.log('Connected to MySQL database!');
+    console.log("Connected to MySQL database!");
   }
 });
 
